c58647ee8eeb80a85953ba032b37207adc4bdfeb
GiorussoP <grpaschoal@gmail.com>
2025-12-08 16:51:13 -0300

    Botões novos

--- src/UI/Screen/MainMenuScreen.cpp
+++ src/UI/Screen/MainMenuScreen.cpp
@@ -6,55 +6,58 @@
 
 #include "AssetLoader.hpp"
 #include "Game.hpp"
+#include "MIDI/MIDIPlayer.hpp"
+#include "scenes/CreditsScene.hpp"
 #include "scenes/Level0.hpp"
 #include "scenes/Level1.hpp"
 #include "scenes/Level2.hpp"
 #include "scenes/Level3.hpp"
-#include "MIDI/MIDIPlayer.hpp"
-#include "scenes/CreditsScene.hpp"
 
 MainMenuScreen::MainMenuScreen(class Game *game, const std::string &fontName)
     : UIScreen(game, fontName) {
 
-  auto mB1 = AddButton(getAssetPath("textures/hud/iniciar.png"), [this] {
-    // Load saved state and go to saved level
-    auto savedState = mGame->LoadState();
-    int savedScene = 0; // Default to level0 (which means start from level1)
-    if (savedState.find("SCENE_STATE") != savedState.end()) {
-      savedScene = savedState["SCENE_STATE"];
-    }
+  auto mB1 =
+      AddButton(getAssetPath("sprites/scenes/startbutton160x40.png"), [this] {
+        // Load saved state and go to saved level
+        auto savedState = mGame->LoadState();
+        int savedScene = 0; // Default to level0 (which means start from level1)
+        if (savedState.find("SCENE_STATE") != savedState.end()) {
+          savedScene = savedState["SCENE_STATE"];
+        }
 
-    // Load the appropriate level
-    // Note: scene0 means start from beginning (Level1)
-    switch (savedScene) {
-    case 0:
-      mGame->LoadScene(new Level0(mGame));
-      break;
-    case 1:
-      mGame->LoadScene(new Level1(mGame));
-      break;
-    case 2:
-      mGame->LoadScene(new Level2(mGame));
-      break;
-    case 3:
-      mGame->LoadScene(new Level3(mGame));
-      break;
-    default:
-      mGame->LoadScene(new Level1(mGame));
-      break;
-    }
-  });
+        // Load the appropriate level
+        // Note: scene0 means start from beginning (Level1)
+        switch (savedScene) {
+        case 0:
+          mGame->LoadScene(new Level0(mGame));
+          break;
+        case 1:
+          mGame->LoadScene(new Level1(mGame));
+          break;
+        case 2:
+          mGame->LoadScene(new Level2(mGame));
+          break;
+        case 3:
+          mGame->LoadScene(new Level3(mGame));
+          break;
+        default:
+          mGame->LoadScene(new Level1(mGame));
+          break;
+        }
+      });
   SDL_Log("Button 1 Added!");
-  mB1->ButtonSetScale(Vector3(0.5f, 0.5f, 0.0f));
+
+  mB1->ButtonSetScale(Vector3(0.8f, 0.5f, 0.0f));
   mB1->ButtonSetPosition(Vector3(0.0f, 0.25f, 0.0f));
 
-  auto mB2 =
-      AddButton(getAssetPath("textures/hud/sair.png"), [this] { mGame->Quit(); });
+  auto mB2 = AddButton(getAssetPath("sprites/scenes/quitbutton160x40.png"),
+                       [this] { mGame->Quit(); });
   SDL_Log("Button 2 Added!");
-  mB2->ButtonSetScale(Vector3(0.5f, 0.5f, 0.0f));
+  mB2->ButtonSetScale(Vector3(0.8f, 0.5f, 0.0f));
   mB2->ButtonSetPosition(Vector3(0.0f, -0.2f, 0.0f));
 
-  auto image = AddImageOrElement(getAssetPath("sprites/scenes/title-screen.png"));
+  auto image =
+      AddImageOrElement(getAssetPath("sprites/scenes/title-screen.png"));
   image->SetPosition(Vector3(0.0f, 0.0f, -1.0f));
   image->SetScale(Vector3(2.0f, 2.0f, 1.0f));
 }
